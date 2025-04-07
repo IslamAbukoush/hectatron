d27c62a32aa3c44b96b7d35aa5ce086ca2f1cb46
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-04-07 11:52:55 +0300

    better header responsiveness

--- hectatron/components/Header.tsx
+++ hectatron/components/Header.tsx
@@ -5,42 +5,49 @@ import { usePathname } from "@/i18n/navigation"
 import { cn } from "@/lib/utils"
 
 interface LinkItem {
-  href: string;
-  label: string;
+    href: string;
+    label: string;
 }
 
 const Header = () => {
     const path = usePathname();
-    
+
     const links: LinkItem[] = [
         { href: '/', label: 'Home' },
         { href: '/about', label: 'About' },
         { href: '/projects', label: 'Projects' },
         { href: '/contact', label: 'Contact' },
     ];
-    
+
     return (
-        <div className="grid grid-cols-[1fr_2fr_1fr] py-8 px-4 z-10">
-            <div className="flex justify-center items-center px-10">
-                <Image src='/images/logo.png' alt="Hectatron" width={300} height={100} className="w-full" />
+        <div className="md:grid md:grid-cols-[1fr_2fr_1fr] flex justify-between items-center py-8 px-4 z-10">
+            <div className="flex md:justify-center justify-start items-center px-0 h-full">
+                <Image src='/images/logo.png' alt="Hectatron" width={300} height={100} className="h-full object-contain" />
             </div>
-            <div className="relative flex justify-around items-center bg-white/11 px-[15%] rounded-[24px]">
+            <div className="relative h-full md:flex hidden justify-around items-center bg-white/11 px-10 rounded-3xl lg:px-16">
                 {links.map((link, i) => (
-                    <Link 
-                        key={i} 
-                        href={link.href} 
+                    <Link
+                        key={i}
+                        href={link.href}
                         className={cn("text-sm text-white opacity-45 py-3", path === link.href && "opacity-100")}
                     >
                         {link.label}
                     </Link>
                 ))}
-                
-                <Image src='/icons/global.svg' alt="language switcher icon" width={25} height={25} className="absolute right-5 cursor-pointer" />
+                <Image src='/icons/global.svg' alt="language switcher icon" width={25} height={25} className="absolute right-2 cursor-pointer" />
             </div>
-            <div className="flex justify-center items-center px-10">
-                <div className="bg-button text-center content-center font-bold text-sm text-white px-10 py-6 rounded-[24px] cursor-pointer border-[1px] border-white/60 shadow-[inset_10px_10px_20px_rgba(255,255,255,0.25),inset_-10px_-10px_20px_rgba(255,255,255,0.25)]">
+            <div className="md:flex hidden justify-center items-center px-10 w-full">
+                <div className="bg-button text-center content-center font-bold text-sm text-white px-10 py-6 rounded-3xl cursor-pointer border border-white/60 shadow-[inset_10px_10px_20px_rgba(255,255,255,0.25),inset_-10px_-10px_20px_rgba(255,255,255,0.25)] text-nowrap">
                     Get Started
                 </div>
+            </div>
+            <div className="h-[60px] w-[60px] relative flex items-center justify-center md:hidden">
+                <Image
+                    src="/icons/menu.svg"
+                    alt="menu icon"
+                    fill
+                    className="object-contain"
+                />
             </div>
         </div>
     )
