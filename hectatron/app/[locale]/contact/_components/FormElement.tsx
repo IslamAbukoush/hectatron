bf6db38acae4c7259b50f1b81cc843fb779a7256
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-04-11 16:17:05 +0300

    contact us page

--- hectatron/app/[locale]/contact/_components/FormElement.tsx
+++ hectatron/app/[locale]/contact/_components/FormElement.tsx
@@ -2,6 +2,7 @@
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import {
     Form,
     FormControl,
@@ -22,9 +23,9 @@ const formSchema = z.object({
     firstName: z.string().min(2).max(50),
     lastName: z.string().min(2).max(50),
     email: z.string().email(),
-    phoneNumber: z.string(),
-    services: z.enum(['service1', 'service2', 'service3', 'service4']),
-    message: z.string(),
+    phoneNumber: z.string().min(7).max(25),
+    services: z.enum(['newWebsite', 'websiteRedisgn', 'websiteBugFixes', 'technicalConsultation']),
+    message: z.string().min(10).max(3000),
 })
 
 export default function FormElement() {
@@ -35,7 +36,7 @@ export default function FormElement() {
             lastName: '',
             email: '',
             phoneNumber: '',
-            services: 'service1',
+            services: 'newWebsite',
             message: '',
         },
     })
@@ -57,9 +58,9 @@ export default function FormElement() {
                 name={type}
                 render={({ field }) => (
                     <FormItem>
-                        <FormLabel>{label}</FormLabel>
+                        <FormLabel className='text-white text-base'>{label}</FormLabel>
                         <FormControl>
-                            <input placeholder={placeholder} {...field} />
+                            <input className='appearance-none outline-none text-white border-b-1 border-[#8D8D8D] py-2' placeholder={placeholder} {...field} />
                         </FormControl>
                         <FormMessage />
                     </FormItem>
@@ -69,14 +70,28 @@ export default function FormElement() {
     }
 
     return (
-        <div className='bg-white'>
+        <div className='p-10'>
             <Form {...form} >
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                    <Item type="firstName" label="First Name" placeholder="John" />
-                    <Item type="lastName" label="Last Name" placeholder="Doe" />
-                    <Item type="email" label="Email" placeholder="johndoe@gmail.com" />
-                    <Item type="phoneNumber" label="Phone Number" placeholder="+1 012 3456 789" />
-                    <button type="submit">Submit</button>
+                    <div className='grid grid-cols-2 gap-5'>
+                        <Item type="firstName" label="First Name" placeholder="John" />
+                        <Item type="lastName" label="Last Name" placeholder="Doe" />
+                        <Item type="email" label="Email" placeholder="johndoe@gmail.com" />
+                        <Item type="phoneNumber" label="Phone Number" placeholder="+1 012 3456 789" />
+                    </div>
+                    <RadioGroup defaultValue="newWebsite" name='services' className='flex flex-col'>
+                        <p className='text-white'>Select Service?</p>
+                        <div className='flex justify-between'>
+                            {['newWebsite', 'websiteRedisgn', 'websiteBugFixes', 'technicalConsultation'].map((item, i) => (
+                                <div className="flex items-center space-x-2" key={i}>
+                                    <RadioGroupItem value={item} id={item} />
+                                    <label htmlFor={item} className='text-white'>{item}</label>
+                                </div>
+                            ))}
+                        </div>
+                    </RadioGroup>
+                    <Item type="message" label="Message" placeholder="Write your message..." />
+                    <button type="submit" className='cursor-pointer text-white rounded-md bg-[#FF8629] px-15 py-5'>Send Message</button>
                 </form>
             </Form>
         </div>
