b102e00d1112c2ef8fdc873c38d3a1305ad617d7
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-04-13 16:02:52 +0300

    Robot responsiveness

--- hectatron/app/[locale]/(main)/page.tsx
+++ hectatron/app/[locale]/(main)/page.tsx
@@ -21,10 +21,18 @@ const Home = () => {
   return (
     <div className="">
       <div className='flex flex-col gap-10 items-center justify-center'>
-        <div className="flex flex-col gap-10 items-center pt-20 md:pt-30 h-[calc(100vh-100px)]">
+        <div className="relative overflow-hidden flex flex-col gap-10 items-center pt-20 md:pt-30 h-[calc(100vh-100px)]">
+          <Image
+            className="absolute h-auto min-w-[350px] md:w-[500px] w-[400px] md:bottom-[-195px] bottom-[-160px] left-0 -z-10"
+            src="/images/robot.png"
+            alt="robot"
+            width={500}
+            height={700}
+            priority
+          />
           <Title title='Elevate Your Brand with Custom Web Solutions' description='We design and develop high-quality, custom websites that enhance your brand and drive results.' />
           <div className="
-                bg-button text-center font-bold text-white border-[1px] border-white/60 shadow-[inset_10px_10px_20px_rgba(255,255,255,0.25),inset_-10px_-10px_20px_rgba(255,255,255,0.25)]
+                z-10 bg-button text-center font-bold text-white border-[1px] border-white/60 shadow-[inset_10px_10px_20px_rgba(255,255,255,0.25),inset_-10px_-10px_20px_rgba(255,255,255,0.25)]
                 lg:text-base lg:px-10 lg:py-6 lg:rounded-[24px]
                 xs:text-sm
                 text-xs px-8 py-4 rounded-[16px]
@@ -33,22 +41,6 @@ const Home = () => {
           </div>
         </div>
         <div className="overflow-hidden absolute top-0 left-0 w-full h-full -z-10">
-          <div className="overflow-hidden absolute top-0 left-0 w-full h-full -z-10">
-            <Image
-              className="absolute
-              xl:w-[900px] xl:h-[850px] xl:top-[20%] xl:left-[-150px]
-              sm:w-[700px] sm:h-[700px] sm:top-[31%] sm:left-[-100px]
-              xs:w-[700px] xs:h-[700px] xs:top-[31%] xs:left-[-100px]
-              min-w-[500px] min-h-[500px] w-[500px] h-[500px] top-[51%] left-[-70px]
-              object-contain"
-              src="/images/robot.png"
-              alt="robot"
-              width={500}
-              height={400}
-              priority
-            />
-          </div>
-
           <Image
             className='absolute top-[0] right-0 hidden md:block'
             src='/images/phone.png'
