51b8a4c199752ee2cd5bf0299aa14b88fb6fd5c9
MaxRoenco <maxroenco2004@gmail.com>
2025-04-18 22:37:47 +0300

    title

--- hectatron/app/[locale]/_components/Title.tsx
+++ hectatron/app/[locale]/_components/Title.tsx
@@ -16,8 +16,8 @@ const Title = ({title, description, className, left = false}: TitleProps) => {
     initial='hidden'
     whileInView='visible'
     viewport={{ once: true, amount: 1}}
-    className={cn(`flex flex-col gap-6 justify-center my-[10px] ${left ? 'items-start' : 'px-1 xxs:px-4 xs:px-10 md:px-14 lg:px-48 xl:px-80'}`, className)}>
-        <h1 className="md:text-6xl xs:text-5xl xxs:text-4xl text-3xl text-center text-gradient">{title}</h1>
+    className={cn(`flex flex-col gap-6 justify-center my-[10px] text-center items-center ${left ? 'items-start' : 'px-1 xxs:px-4 xs:px-10 md:px-14 lg:px-48 xl:px-80'}`, className)}>
+        <h1 className="md:text-6xl xs:text-5xl xxs:text-4xl text-3xl text-center text-gradient w-fit">{title}</h1>
         <p className="xl:text-xl lg:text-lg md:text-base xs:text-base text-sm text-white/60 text-center ">{description}</p>
     </m.div>
   )
