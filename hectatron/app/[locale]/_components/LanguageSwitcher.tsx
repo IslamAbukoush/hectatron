074105f099d2e96248848dac55e7dde0dad800ad
MaxRoenco <maxroenco2004@gmail.com>
2025-04-18 13:07:12 +0300

    language swithcer animation

--- hectatron/app/[locale]/_components/LanguageSwitcher.tsx
+++ hectatron/app/[locale]/_components/LanguageSwitcher.tsx
@@ -5,6 +5,7 @@ import { Link } from "@/i18n/navigation";
 import { usePathname } from "@/i18n/navigation";
 import useComponentVisible from "@/lib/hooks/useComponentVisible";
 import { useLocale } from "next-intl";
+import * as m from 'motion/react-m'
 
 export default function LanguageSwitcher() {
     const path = usePathname();
@@ -21,7 +22,11 @@ export default function LanguageSwitcher() {
                 <Image src='/icons/global.svg' alt="language switcher icon" width={25} height={25} />
                 <p className="text-white">{locale}</p>
             </div>
-            {isComponentVisible && <div ref={ref as React.RefObject<HTMLDivElement>} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[100px] h-[100px] bg-[#D86A1580] rounded-full flex justify-center items-center z-40">
+            {isComponentVisible && <m.div 
+            initial={{rotate: -180, opacity: 0.4, scale: 0}}
+            animate={{rotate: 0, opacity: 1, scale: 1}}
+            transition={{duration: 0.3, ease: 'easeInOut'}}
+            ref={ref as React.RefObject<HTMLDivElement>} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[100px] h-[100px] bg-[#D86A1580] rounded-full flex justify-center items-center z-40">
                 <Image src='/icons/global.svg' alt="language switcher icon" width={25} height={25} className="brightness-[150%]" />
                 <ul className="z-50">
                     <li className={cn("absolute top-1/2 left-1/2 translate-x-[calc(-50%+30px)] -translate-y-1/2 text-white", {'bg-[#FFFFFF66] px-1 rounded-2xl': locale === 'EN'})}><Link locale="en" href={path}>EN</Link></li>
@@ -29,7 +34,7 @@ export default function LanguageSwitcher() {
                     <li className={cn("absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-[calc(-50%+30px)] text-white", {'bg-[#FFFFFF66] px-1 rounded-2xl': locale === 'RO'})}><Link locale="ro" href={path}>RO</Link></li>
                     <li className={cn("absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-[calc(-50%-30px)] text-white", {'bg-[#FFFFFF66] px-1 rounded-2xl': locale === 'AR'})}><Link locale="ar" href={path}>AR</Link></li>
                 </ul>
-            </div>}
+            </m.div>}
         </div>
 
     )
