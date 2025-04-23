d3ede950a5bc7f6992621dd827c59d7b4168716b
MaxRoenco <maxroenco2004@gmail.com>
2025-04-23 16:09:53 +0300

    fix titile

--- hectatron/app/[locale]/_components/Title.tsx
+++ hectatron/app/[locale]/_components/Title.tsx
@@ -28,7 +28,7 @@ const Title = ({title, description, className, left = false}: TitleProps) => {
       scale: 1,
     }}
     transition={{ duration: 0.5, ease: 'easeInOut' }}
-    viewport={{ once: true, amount: 1}}
+    viewport={{ once: true }}
     className={cn(`flex flex-col gap-6 justify-center my-[10px] text-center items-center ${left ? 'items-start' : 'px-1 xxs:px-4 xs:px-10 md:px-14 lg:px-48 xl:px-80'}`, className)}>
         <h1 className="md:text-6xl xs:text-5xl xxs:text-4xl text-3xl text-center text-gradient w-fit">{title}</h1>
         <p className="xl:text-xl lg:text-lg md:text-base xs:text-base text-sm text-white/60 text-center ">{description}</p>
